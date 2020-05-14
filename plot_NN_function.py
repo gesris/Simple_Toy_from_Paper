41e374a2c9e0c87ba445008f323f306bb9c46723
gesris <gessi.risto@gmx.de>
2020-05-14 13:32:10 +0200

    better plot design

--- plot_NN_function.py
+++ plot_NN_function.py
@@ -1,49 +1,54 @@
 import numpy as np
 np.random.seed(1234)
 import tensorflow as tf
-tf.random.set_seed(1234)   
-import tensorflow_probability as tfp
+tf.random.set_seed(1234)
 import pickle
 import matplotlib
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+matplotlib.rc("font", size=16, family="serif")
 from tqdm import tqdm
 
 ## Load Data
 model = tf.keras.models.load_model('./mymodel')
 plot_label = pickle.load(open("plot_label.pickle", "rb"))
 
+
 # Create meshgrid for 2D plot
 length = 1000
 sensibility = 0.005
-b = np.linspace(-3, 3, length)
+b = np.linspace(-3, 5, length)
 xx, yy = np.meshgrid(b, b, sparse=False)
 x = np.vstack([xx.reshape((length * length)), yy.reshape((length * length))]).T
 c = tf.squeeze(model(x)).numpy()
 c = c.reshape((length, length))
 
+
 # visualize decision boundary
+print("\nPlotting NN function\n")
 boundary = tf.squeeze(model(x)).numpy()
 boundary = boundary.reshape((length, length))
 # iterate through c to highlight decision boundary
-for y in range(0, length):
+for y in tqdm(range(0, length)):
     for x in range(0, length):
         if c[y][x] > 0.5-sensibility and c[y][x] < 0.5 + sensibility:
             boundary[y][x] = 1
         else:
             boundary[y][x] = 0
 
+
 # Plot NN function
-print("\nPlotting NN function\n")
 limit = [-3, 3]
 plt.figure(figsize=(7, 6))
+plt.grid()
 cbar = plt.contourf(xx, yy, c + boundary, levels=np.linspace(c.min(), c.max(), 21))
-plt.colorbar(cbar)
-plt.xlabel("x1")
-plt.ylabel("x2")
-plt.title("Neural network function")
-plt.tight_layout()
+cbar = plt.colorbar(format="%.1f")
+cbar.set_label("$f$")
+plt.xlabel("$x_1$")
+plt.ylabel("$x_2$")
 plt.xlim(limit[0], limit[1])
 plt.ylim(limit[0], limit[1])
+#ticks = [-2, 0, 2, 4]
+#plt.gca().set_xticks(ticks)
+#plt.gca().set_yticks(ticks)
 plt.savefig("./plots/NN_function_{}.png".format(plot_label), bbox_inches = "tight")
-
+#plt.show()
--- plot_histogram.py
+++ plot_histogram.py
@@ -1,13 +1,8 @@
 import numpy as np
-np.random.seed(1234)
-import tensorflow as tf
-tf.random.set_seed(1234)   
-import tensorflow_probability as tfp
 import pickle
 import matplotlib
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from tqdm import tqdm
+matplotlib.rc("font", size=16, family="serif")
 
 
 ####
@@ -18,10 +13,16 @@ from tqdm import tqdm
 s, b, b_up, b_down, bins = pickle.load(open("plot_histogram.pickle", "rb"))
 plot_label = pickle.load(open("plot_label.pickle", "rb"))
 
-## Scale data
-if("SD_with" in plot_label):
-    s = 3 * s
-    b = b + b_up + b_down
+# Save shifts to disk
+for h, name in [
+        (bins, "bins"),
+        (s, "sig"),
+        (b, "bkg"),
+        (b_up, "bkg_up"),
+        (b_down, "bkg_down")]:
+    np.savetxt(name + ".csv", h)
+
+
 
 
 n = len(s)
@@ -31,12 +32,20 @@ for i in range(0, len(bins) - 1):
 border = 0.5
 
 print("\nPlotting histogram\n")
-
-plt.figure(figsize=(7, 6))
-plt.hist(bins_for_plots_middle, weights= [s[0], s[1]], bins= bins, histtype="step", label="Signal", lw=2)
-plt.hist(bins_for_plots_middle, weights= [b[0], b[1]], bins= bins, histtype="step", label="Backgorund", lw=2)
-plt.legend(loc= "lower center")
-plt.xlabel("Projection with decision boundary from NN at {}".format(border))
-plt.ylabel("# Events")
-plt.axvline(x=border, ymin=0, ymax=n, color="r", linestyle= "dashed", lw=2)
-plt.savefig("./plots/histogram_{}.png".format(plot_label), bbox_inches = "tight")
+lw = 2
+plt.figure(figsize=(6, 6))
+plt.hist(bins_for_plots_middle, weights= [s[0], s[1]], bins= bins, histtype="step", label="Signal", lw=2, color="C0")
+plt.hist(bins_for_plots_middle, weights= [b[0], b[1]], bins= bins, histtype="step", label="Backgorund", lw=2, color="C1")
+plt.hist(bins_for_plots_middle, weights= [b_up[0], b_up[1]], bins= bins, ls="--", histtype="step", label="Backgorund", lw=2, color="C1")
+plt.hist(bins_for_plots_middle, weights= [b_down[0], b_down[1]], bins= bins, ls=":", histtype="step", label="Backgorund", lw=2, color="C1")
+plt.plot([0], [0], ls="-", lw=lw, color="C0", label="Signal")
+plt.plot([0], [0], ls="-", lw=lw, color="C1", label="Background")
+plt.plot([0], [0], ls="--", lw=lw, color="C1", label="Bkg. (up-shift)")
+plt.plot([0], [0], ls=":", lw=lw, color="C1", label="Bkg. (down-shift)")
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0., prop={'size': 14})
+plt.xlabel("$f$")
+plt.ylabel("Count")
+plt.xlim(bins[0], bins[-1])
+#plt.axvline(x=border, ymin=0, ymax=n, color="r", linestyle= "dashed", lw=2)
+#plt.savefig("./plots/histogram_{}.png".format(plot_label), bbox_inches = "tight")
+#plt.show()
