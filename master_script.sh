da76fa6d08a30701d91974ccce3d620b990b20cf
gesris <gessi.risto@gmx.de>
2020-05-16 01:17:14 +0200

    j

--- master_script.sh
+++ master_script.sh
@@ -23,6 +23,6 @@ git push origin master
 ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && git pull && sh master_plot.sh'
 
 scp bms3:/home/gristo/Simple_Toy_from_Paper/plots/* /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/
-scp bms3:/home/gristo/Simple_Toy_from_Paper/*.png /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/
+scp bms3:/home/gristo/Simple_Toy_from_Paper/*.png /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/plot_nll_$PLOTLABEL.png
 
 ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && sh cleanup_plots.sh'
