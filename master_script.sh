7c01ff86e6782c8013565af7a2b3ed2e39509ebe
gesris <gessi.risto@gmx.de>
2020-05-16 00:53:42 +0200

    master set up

--- master_script.sh
+++ master_script.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && sh master_plot.sh'
+read -p "Enter Background X-Shift: " XSHIFT
+read -p "Enter Background Y-Shift: " YSHIFT
+read -p "Enter Shift Scale: " SHIFTSCALE
+read -p "Enter Plot Label including CE, SN_no or SD_with: " PLOTLABEL
 
-scp bms3:/home/gristo/Simple_Toy_from_Paper/plots/* /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/
-scp bms3:/home/gristo/Simple_Toy_from_Paper/*.png /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/
+
+#ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && sed -i "/shift_scale = .*/c\    shift_scale = $SHIFTSCALE" ./create_data.py'
+#ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && sed -i "/shift = shift_scale [*] .*/c\    shift = shift_scale * np.array([$XSHIFT, $YSHIFT])" ./create_data.py'
+#ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && sed -i "/plot_label = .*/c\    plot_label = \"$PLOTLABEL\"" ./create_data.py'
+
+#ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && cat ./create_data.py'
+
+
+sed -i "/shift_scale = .*/c\    shift_scale = $SHIFTSCALE" /home/risto/Masterarbeit/Simple_Toy_from_Paper/create_data.py
+sed -i "/shift = shift_scale [*] .*/c\    shift = shift_scale * np.array([$XSHIFT, $YSHIFT])" /home/risto/Masterarbeit/Simple_Toy_from_Paper/create_data.py
+sed -i "/plot_label = .*/c\    plot_label = '$PLOTLABEL'" /home/risto/Masterarbeit/Simple_Toy_from_Paper/create_data.py
+
+git commit -m "changing data for different plots" create_data.py
+git push origin master
+
+ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && git pull'
+#ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && git pull && sh master_plot.sh'
+
+#scp bms3:/home/gristo/Simple_Toy_from_Paper/plots/* /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/
+#scp bms3:/home/gristo/Simple_Toy_from_Paper/*.png /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/
