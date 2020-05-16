18ff876885b5e54bb1e4abff7bb163ee57a7a167
gesris <gessi.risto@gmx.de>
2020-05-16 02:31:20 +0200

    j

--- master_script.sh
+++ master_script.sh
@@ -4,22 +4,21 @@
 while IFS=, read -r XSHIFT YSHIFT SHIFTSCALE PLOTLABEL
 do
 
-    ssh -t gristo@bms3 'sed -i "/shift_scale = .*/c\    shift_scale = $SHIFTSCALE" ./create_data.py'
-    ssh -t gristo@bms3 'sed -i "/shift = shift_scale [*] .*/c\    shift = shift_scale * np.array([$XSHIFT, $YSHIFT])" ./create_data.py'
-    ssh -t gristo@bms3 'sed -i "/plot_label = .*/c\    plot_label = \"$PLOTLABEL\"" ./create_data.py'
+    sed -i "/shift_scale = .*/c\    shift_scale = $SHIFTSCALE" ./create_data.py
+    sed -i "/shift = shift_scale [*] .*/c\    shift = shift_scale * np.array([$XSHIFT, $YSHIFT])" ./create_data.py
+    sed -i "/plot_label = .*/c\    plot_label = \"$PLOTLABEL\"" ./create_data.py
 
-    #git commit -m "changing data for different plots" create_data.py
-    #git push origin master
+    git commit -m "changing data for different plots" create_data.py
+    git push origin master
 
-    #ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && git pull && sh master_plot.sh'
-    ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && sh master_plot.sh'
+    ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && git pull && sh master_plot.sh'
 
     scp bms3:/home/gristo/Simple_Toy_from_Paper/plots/* /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/
     scp bms3:/home/gristo/Simple_Toy_from_Paper/*.png /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/plot_nll_$PLOTLABEL.png
 
     ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && sh cleanup_plots.sh'
 
-done < ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && cat input.csv'
+done < input.csv
 
 #read -p "Enter Background X-Shift: " XSHIFT
 #read -p "Enter Background Y-Shift: " YSHIFT
