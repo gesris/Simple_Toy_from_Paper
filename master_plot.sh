9d408eb2de614d51671ac010361566eb6a637e38
gesris <gessi.risto@gmx.de>
2020-06-28 22:59:12 +0200

    bugfixes

--- master_plot.sh
+++ master_plot.sh
@@ -2,7 +2,12 @@
 
 source /home/gristo/Simple_Toy_from_Paper/venv/bin/activate
 
-sh /home/gristo/Simple_Toy_from_Paper/make_plots.sh
+cd /home/gristo/Simple_Toy_from_Paper/
+
+python3 create_data.py
+python3 train_data.py
+python3 plot_histogram.py
+python3 plot_NN_function.py
 
 echo " "
 echo "Initiating mu-fit."
