b4018b0400d9adb75d8e81eadb653265a32f491b
gesris <gessi.risto@gmx.de>
2020-07-01 00:44:43 +0200

    changed signal and background scale - applied scaling factors of mlnllanalysis

--- train_data.py
+++ train_data.py
@@ -302,10 +302,14 @@ def main(loss):
 
     model.save('./mymodel')
 
-    s = hist(tf.squeeze(model(x_signal_noshift)), bins) * (10. / 25000.)
-    b = hist(tf.squeeze(model(x_background_noshift)), bins) * (1000. / 25000.)
-    b_up = hist(tf.squeeze(model(x_background_upshift)), bins) * (1000. / 25000.)
-    b_down = hist(tf.squeeze(model(x_background_downshift)), bins) * (1000. / 25000.)
+    #s = hist(tf.squeeze(model(x_signal_noshift)), bins) * (10. / 25000.)
+    #b = hist(tf.squeeze(model(x_background_noshift)), bins) * (1000. / 25000.)
+    #b_up = hist(tf.squeeze(model(x_background_upshift)), bins) * (1000. / 25000.)
+    #b_down = hist(tf.squeeze(model(x_background_downshift)), bins) * (1000. / 25000.)
+    s = hist(tf.squeeze(model(x_signal_noshift)), bins) * (2396. / 25000.)
+    b = hist(tf.squeeze(model(x_background_noshift)), bins) * (458504. / 25000.)
+    b_up = hist(tf.squeeze(model(x_background_upshift)), bins) * (458504. / 25000.)
+    b_down = hist(tf.squeeze(model(x_background_downshift)), bins) * (458504. / 25000.)
 
     print("SIGNAL:           {}, SUM:   {}".format(s, np.sum(s)))
     print("BACKGROUND:       {}, SUM:   {}".format(b, np.sum(b)))
