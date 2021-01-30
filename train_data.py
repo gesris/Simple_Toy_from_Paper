e6b88b3ab2eed4244e1284482760162eab74909d
gesris <gessi.risto@gmx.de>
2021-01-30 22:25:55 +0100

    bugfixes

--- train_data.py
+++ train_data.py
@@ -19,7 +19,7 @@ from tqdm import tqdm
 def binfunction(x, right_edge, left_edge):
     # tf.cast casts a tensor to a new type -> here float32, since default is double
     # tf.squeeze removes dimensions of size 1 from the shape of a tensor
-    y = tf.squeeze(
+    mask = tf.squeeze(
         tf.cast(
             tf.cast(x > left_edge, tf.float32) * tf.cast(x <= right_edge, tf.float32), tf.float32
             )
@@ -33,7 +33,7 @@ def binfunction(x, right_edge, left_edge):
         g = -1.0 * gauss * (x - mid) / sigma**2
         g = tf.squeeze(g) * tf.squeeze(dy)
         return (g, None, None)
-    return y, grad
+    return mask, grad
 
 
 def main(loss):
@@ -42,20 +42,19 @@ def main(loss):
     #### Loading data and splitting into training and validation with same size
     ####
 
-    x_noshift_signal, x_upshift_signal, x_downshift_signal,\
-    x_noshift_background, x_upshift_background, x_downshift_background,\
-    y, w = pickle.load(open("train.pickle", "rb"))
+    x, x_up, x_down, y, w = pickle.load(open("train.pickle", "rb"))
+
+    # Compute class weights for CE loss
+    w_sumsig = np.sum(w[y == 1])
+    w_sumbkg = np.sum(w[y == 0])
+    w_class = np.zeros(w.shape)
+    w_class[y == 1] = (w_sumsig + w_sumbkg) / w_sumsig
+    w_class[y == 0] = (w_sumsig + w_sumbkg) / w_sumbkg
     
-    x_train_noshift_signal, x_val_noshift_signal,\
-    x_train_upshift_signal, x_val_upshift_signal,\
-    x_train_downshift_signal, x_val_downshift_signal,\
-    x_train_noshift_background, x_val_noshift_background,\
-    x_train_upshift_background, x_val_upshift_background,\
-    x_train_downshift_background, x_val_downshift_background = train_test_split(
-        x_noshift_signal, x_upshift_signal, x_downshift_signal,\
-        x_noshift_background, x_upshift_background, x_downshift_background,\
-        test_size=0.5, random_state=1234
-    )
+
+    ## Make training and validation datasets
+    x_train, x_val, x_up_train, x_up_val, x_down_train, x_down_val, y_train, y_val, w_train, w_val, w_class_train, w_class_val = train_test_split(
+        x, x_up, x_down, y, w, w_class, test_size=0.5, random_state=1234)
     
 
     ####
@@ -67,31 +66,34 @@ def main(loss):
     tf.keras.layers.Dense(1, activation=tf.sigmoid)
     ])
 
+    batch_size = x_train.shape[0]
+    batch_scale = tf.constant(2.0, tf.float32)
     batch_len = None
 
     # Training data
-    x_signal_noshift = tf.Variable(x_train_noshift_signal, tf.float32, shape=[batch_len, 2])
-    x_background_noshift = tf.Variable(x_train_noshift_background, tf.float32, shape=[batch_len, 2])
-    x_background_upshift = tf.Variable(x_train_upshift_background, tf.float32, shape=[batch_len, 2])
-    x_background_downshift = tf.Variable(x_train_downshift_background, tf.float32, shape=[batch_len, 2])
+    x = tf.Variable(x_train, tf.float32, shape=[batch_len, 2])
+    x_up = tf.Variable(x_up_train, tf.float32, shape=[batch_len, 2])
+    x_down = tf.Variable(x_down_train, tf.float32, shape=[batch_len, 2])
 
     # Validation data
-    x_signal_noshift_val = tf.Variable(x_val_noshift_signal, tf.float32, shape=[batch_len, 2])
-    x_background_noshift_val = tf.Variable(x_val_noshift_background, tf.float32, shape=[batch_len, 2])
-    x_background_upshift_val = tf.Variable(x_val_upshift_background, tf.float32, shape=[batch_len, 2])
-    x_background_downshift_val = tf.Variable(x_val_downshift_background, tf.float32, shape=[batch_len, 2])
+    x_val = tf.Variable(x_val, tf.float32, shape=[batch_len, 2])
+    x_up_val = tf.Variable(x_up_val, tf.float32, shape=[batch_len, 2])
+    x_down_val = tf.Variable(x_down_val, tf.float32, shape=[batch_len, 2])
+
+    ## Class weights
+    w = tf.placeholder(tf.float32, shape=[batch_len])
+    w_class = tf.placeholder(tf.float32, shape=[batch_len])
 
     
     ####
     #### Define losses
     ####
     
-    bins = np.linspace(0.0, 1.0, 9)
+    bins = np.linspace(0, 1, 9)
     bin_edges = bins
     right_edges = bin_edges[1:] # all except the first
     left_edges = bin_edges[:-1] # all except the last
     mask_algo = binfunction
-    batch_scale = tf.constant(2.0, tf.float32)
     
     # assign value to tensor variables
     ### default: mu = 1, theta = 0
@@ -104,55 +106,54 @@ def main(loss):
     one = tf.constant(1.0, tf.float32)
 
 
-    ####
-    #### First define Loss, then define gradient, then minimize via grad(loss, model.variables)
-    ####
-
-    def hist(x, bins):
-        counts = []
-        # splits histogram in bins regarding their left and right edges
-        # zip function puts left and right edge together in one iterable array
-        for right_edge, left_edge in zip(bins[1:], bins[:-1]):
-            # sums up all 1 entries of each bin 
-            counts.append(tf.reduce_sum(binfunction(x, right_edge, left_edge)))
-        return tf.squeeze(tf.stack(counts))
-
-
-    ## Negative Log Likelihood loss with nuisance
-    def loss_nll(model, x_sig, x_bkg, x_bkg_up, x_bkg_down, parameters, nuisance_is_true):
+    ## Calculationg NLL
+    def loss_nll(model, x, y, w, right_edges, left_edges, mu, theta, nuisance_is_true):
         nll0 = null
-        # parameters = [mu, theta]
 
-        ## Histograms of events separated by decision boundary
-        sig = hist(tf.squeeze(model(x_sig)), bins)
-        bkg = hist(tf.squeeze(model(x_bkg)), bins)
-        bkg_up = hist(tf.squeeze(model(x_bkg_up)), bins)
-        bkg_down = hist(tf.squeeze(model(x_bkg_down)), bins)
+        for i, right_edge, left_edge in zip(range(len(left_edges)), right_edges, left_edges):
+            print("Bin (Right edge, left edge, mid): {:g} / {:g} / {:g}".format(
+                right_edge, left_edge, left_edge + 0.5 * (right_edge - left_edge)))
+            right_edge_ = tf.constant(right_edge, tf.float32)
+            left_edge_ = tf.constant(left_edge, tf.float32)
 
-        ## Calculate NLL with or without nuisance
-        for i in range(0, len(sig)):
+            
+            ## Nominal
+            mask = mask_algo(model(x), right_edge_, left_edge_)
+            sig = tf.reduce_sum(mask * y * w * batch_scale)
+            bkg = tf.reduce_sum(mask * (one - y) * w * batch_scale)
+
+
+            ## Shifts
+            mask_up = mask_algo(model(x_up), right_edge_, left_edge_)
+            bkg_up = tf.reduce_sum(mask_up * (one - y) * w * batch_scale)
+
+            mask_down = mask_algo(model(x_down), right_edge_, left_edge_)
+            bkg_down = tf.reduce_sum(mask_down * (one - y) * w * batch_scale)
+
+
             if(nuisance_is_true):
-                sys = tf.maximum(parameters[1], null) * (bkg_up[i] - bkg[i]) \
-                + tf.minimum(parameters[1], null) * (bkg[i] - bkg_down[i])
+                sys = tf.maximum(theta, null) * (bkg_up - bkg) + tf.minimum(theta, null) * (bkg - bkg_down)
+                exp = mu * sig + bkg
+                obs = sig + bkg
+                nll0 -= tfp.distributions.Poisson(tf.maximum(exp + sys, epsilon)).log_prob(tf.maximum(obs, epsilon))
             else:
                 sys = tf.constant(0.0, dtype=tf.float32)
-            exp = parameters[0] * sig[i] + bkg[i]
-            obs = sig[i] + bkg[i]
-
-            nll0 -= tfp.distributions.Poisson(tf.maximum(exp + sys, epsilon)).log_prob(tf.maximum(obs, epsilon))
-        
+                exp = mu * sig + bkg
+                obs = sig + bkg
+                nll0 -= tfp.distributions.Poisson(tf.maximum(exp + sys, epsilon)).log_prob(tf.maximum(obs, epsilon))
+            
         if(nuisance_is_true):
             ## Normalized Gaussian constraining the nuisance
-            nll0 -= tfp.distributions.Normal(loc=0, scale=1).log_prob(parameters[1])
+            nll0 -= tfp.distributions.Normal(loc=0, scale=1).log_prob(theta)
         return nll0
 
 
     ## Standard Deviation loss with and without nuisance
-    def loss_sd(model, x_sig, x_bkg, x_bkg_up, x_bkg_down, parameters, nuisance_is_true):
+    def loss_sd(nll0, parameters, nuisance_is_true):
         with tf.GradientTape(persistent=True) as second_order:
             with tf.GradientTape() as first_order:
                 if(nuisance_is_true):
-                    loss_value_nll = loss_nll(model, x_sig, x_bkg, x_bkg_up, x_bkg_down, parameters, nuisance_is_true)
+                    loss_value_nll = nll0
                     gradnll = first_order.gradient(loss_value_nll, parameters)
                     hessian_rows = [second_order.gradient(g, parameters) for g in tf.unstack(gradnll)]
                     hessian_matrix = tf.stack(hessian_rows, axis=-1)
@@ -161,7 +162,7 @@ def main(loss):
                     standard_deviation = tf.math.sqrt(poi)
                 else:
                     mu_sd_no_nuisance = parameters[0]
-                    loss_value_nll = loss_nll(model, x_sig, x_bkg, x_bkg_up, x_bkg_down, parameters, nuisance_is_true)
+                    loss_value_nll = nll0
                     gradnll = first_order.gradient(loss_value_nll, mu_sd_no_nuisance)
                     gradgradnll = second_order.gradient(gradnll, mu_sd_no_nuisance)
                     covariance = 1 / gradgradnll
@@ -169,36 +170,28 @@ def main(loss):
         return standard_deviation
 
 
-    def grad_sd(model, x_sig, x_bkg, x_bkg_up, x_bkg_down, parameters, nuisance_is_true):
+    def grad_sd(nll0, parameters, nuisance_is_true):
         with tf.GradientTape() as backprop:
-            loss_value = loss_sd(model, x_sig, x_bkg, x_bkg_up, x_bkg_down, parameters, nuisance_is_true)
+            loss_value = loss_sd(nll0, parameters, nuisance_is_true)
             backpropagation = backprop.gradient(loss_value, model.trainable_variables)
         return backpropagation
 
 
     ## Cross Entropy Loss
-    def loss_ce(model, x_sig, x_bkg):
-        f_sig = model(x_sig)
-        f_bkg = model(x_bkg)
+    def loss_ce(model, x, y):
+        f = model(x)
         ce_loss = -tf.math.reduce_mean(
-            tf.math.log(tf.maximum(f_sig, 1e-9)) + tf.math.log(tf.maximum((1 - f_bkg), 1e-9))
+            tf.math.log(y * tf.maximum(f, epsilon)) + (one - y) * tf.math.log(tf.maximum((1 - f), epsilon))
         )
         return ce_loss
 
 
-    def grad_ce(model, x_sig, x_bkg):
+    def grad_ce(model, x, y):
         with tf.GradientTape() as backprop:
-            loss_value = loss_ce(model, x_sig, x_bkg)
+            loss_value = loss_ce(model, x, y)
             backpropagation = backprop.gradient(loss_value, model.trainable_variables)
         return backpropagation
-
     
-    ####
-    #### Choose optimizer for training
-    ####
-
-    optimizer = tf.keras.optimizers.Adam()
-
     
     ####
     #### Pretraining decisions
@@ -215,23 +208,33 @@ def main(loss):
     else:
         pass
 
+    nll0 = loss_nll(model, x, y, w, right_edges, left_edges, mu, theta, nuisance_is_true)
+    nll0_val = loss_nll(model, x_val, y_val, w_val, right_edges, left_edges, mu, theta, nuisance_is_true)
+
+    
+    ####
+    #### Choose optimizer for training
+    ####
+
+    optimizer = tf.keras.optimizers.Adam()
+
 
     ## Summery of possible losses
     def model_loss_and_grads(loss):
         if(loss == "Cross Entropy Loss"):
-            model_loss      = loss_ce(model, x_signal_noshift, x_background_noshift)
-            model_loss_val  = loss_ce(model, x_signal_noshift_val, x_background_noshift_val)
-            model_grads     = grad_ce(model, x_signal_noshift, x_background_noshift)
+            model_loss      = loss_ce(model, x, y)
+            model_loss_val  = loss_ce(model, x_val, y_val)
+            model_grads     = grad_ce(model, x, y)
 
         elif(loss == "Standard Deviation Loss"):
-            model_loss      = loss_sd(model, x_signal_noshift, x_background_noshift, x_background_upshift, x_background_downshift, [mu, theta], nuisance_is_true)
-            model_loss_val  = loss_sd(model, x_signal_noshift_val, x_background_noshift_val, x_background_upshift_val, x_background_downshift_val, [mu, theta], nuisance_is_true)
-            model_grads     = grad_sd(model, x_signal_noshift, x_background_noshift, x_background_upshift, x_background_downshift, [mu, theta], nuisance_is_true)
+            model_loss      = loss_sd(nll0, [mu, theta], nuisance_is_true)
+            model_loss_val  = loss_sd(nll0_val, [mu, theta], nuisance_is_true)
+            model_grads     = grad_sd(nll0, [mu, theta], nuisance_is_true)
 
         elif(loss == "Standard Deviation Loss with nuisance"):
-            model_loss      = loss_sd(model, x_signal_noshift, x_background_noshift, x_background_upshift, x_background_downshift, [mu, theta], nuisance_is_true)
-            model_loss_val  = loss_sd(model, x_signal_noshift_val, x_background_noshift_val, x_background_upshift_val, x_background_downshift_val, [mu, theta], nuisance_is_true)
-            model_grads     = grad_sd(model, x_signal_noshift, x_background_noshift, x_background_upshift, x_background_downshift, [mu, theta], nuisance_is_true)
+            model_loss      = loss_sd(nll0, [mu, theta], nuisance_is_true)
+            model_loss_val  = loss_sd(nll0_val, [mu, theta], nuisance_is_true)
+            model_grads     = grad_sd(nll0, [mu, theta], nuisance_is_true)
 
         return model_loss, model_loss_val, model_grads
 
@@ -246,9 +249,9 @@ def main(loss):
     ######################\n\
     # Warmup Initialized #\n\
     ######################\n")
-        for warmup_step in tqdm(range(0, 70)):
+        for warmup_step in tqdm(range(0, 30)):
             ## Warmup trains model without nuisance to increase stability
-            grads = grad_sd(model, x_signal_noshift, x_background_noshift, x_background_upshift, x_background_downshift, [mu, theta], False)    # nuisance has to be FALSE here
+            grads = grad_sd(nll0, [mu, theta], False)    # nuisance has to be FALSE here
             optimizer.apply_gradients(zip(grads, model.trainable_variables))
     
 
@@ -258,6 +261,7 @@ def main(loss):
 
     ## prerequisites for training
     steps = []
+    max_steps = 1000
     loss_train_list = []
     loss_validation_list = []
     max_patience = 30
@@ -267,7 +271,7 @@ def main(loss):
     min_loss, _, _ = model_loss_and_grads(loss)
 
     ## Training loop
-    for epoch in range(1, 1000):
+    for epoch in range(1, max_steps):
         current_loss, current_loss_val, grads = model_loss_and_grads(loss)
 
         ## apply grads and vars
@@ -298,10 +302,29 @@ def main(loss):
 
     model.save('./mymodel')
 
-    s = hist(tf.squeeze(model(x_signal_noshift)), bins) * (50. / 25000.)
-    b = hist(tf.squeeze(model(x_background_noshift)), bins) * (1000. / 25000.)
-    b_up = hist(tf.squeeze(model(x_background_upshift)), bins) * (1000. / 25000.)
-    b_down = hist(tf.squeeze(model(x_background_downshift)), bins) * (1000. / 25000.)
+
+    s =[]
+    b = []
+    b_up = []
+    b_down = []
+
+    for i, right_edge, left_edge in zip(range(len(left_edges)), right_edges, left_edges):
+        right_edge_ = tf.constant(right_edge, tf.float32)
+        left_edge_ = tf.constant(left_edge, tf.float32)
+
+        
+        ## Nominal
+        mask = mask_algo(model(x), right_edge_, left_edge_)
+        s.append(tf.reduce_sum(mask * y * w * batch_scale))
+        b.append(tf.reduce_sum(mask * (one - y) * w * batch_scale))
+
+
+        ## Shifts
+        mask_up = mask_algo(model(x_up), right_edge_, left_edge_)
+        b_up.append(tf.reduce_sum(mask_up * (one - y) * w * batch_scale))
+
+        mask_down = mask_algo(model(x_down), right_edge_, left_edge_)
+        b_down.append(tf.reduce_sum(mask_down * (one - y) * w * batch_scale))
 
     pickle.dump([s, b, b_up, b_down, bins], open("plot_histogram.pickle", "wb"))
 
