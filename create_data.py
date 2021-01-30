81976560a2703b600df8f8162ff6e9c4c1bb440c
gesris <gessi.risto@gmx.de>
2021-01-30 22:25:55 +0100

    changing data for different plots

--- create_data.py
+++ create_data.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib
+matplotlib.rc("font", size=16, family="serif")
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-matplotlib.rc("font", size=16, family="serif")
 np.random.seed(1234)
 import pickle
 
 
 # Make signal and background samples with up and down shifts in background process
 def make_sig(num_events):
-    mean = [-1, -1]
-    corr = 0.0
+    mean = [0, 0]
+    corr = -0.0
     cov = [[1.0, corr], [corr, 1.0]]
     return np.random.multivariate_normal(mean, cov, num_events)
 
@@ -24,14 +24,12 @@ def make_bkg(num_events, shift):
 
 
 def make_dataset(num_events, shift):
-    num_events = num_events // 2
-    x1 = np.vstack(make_sig(num_events))
-    x2 = np.vstack(make_bkg(num_events, shift))
+    num_events = num_events
+    x = np.vstack([make_sig(num_events), make_bkg(num_events, shift)])
     y = np.hstack([np.ones(num_events), np.zeros(num_events)])
-    return x1, x2, y
+    return x, y
 
 def main(shift_scale, shift, plot_label):
-        
 
     ####
     #### Create training dataset - important parameters for plots!
@@ -40,32 +38,46 @@ def main(shift_scale, shift, plot_label):
     num_train = 100000
     signal_exp = 50
     background_exp = 1000
-    signal_scale = signal_exp / float(num_train / 4.)
-    background_scale = background_exp / float(num_train / 4.)
 
 
     print("\nCreating data with shift: {}, shift scale: {}, plot label: {}".format(shift, shift_scale, plot_label))
 
-    # dataset with events containing each x-/y-coordinates
-    x_train_noshift_signal, x_train_noshift_background, y_train = make_dataset(num_train, 0)
-    x_train_upshift_signal, x_train_upshift_background, _ = make_dataset(num_train, shift)
-    x_train_downshift_signal, x_train_downshift_background, _ = make_dataset(num_train, -shift)
+    # Training dataset
+    x_train, y_train = make_dataset(num_train, 0)
+    x_train_up, _ = make_dataset(num_train, shift)
+    x_train_down, _ = make_dataset(num_train, -shift)
 
 
-    # weight for normalization
+    ##  weight for scaling
+    signal_scale = signal_exp / float(num_train)
+    background_scale = background_exp / float(num_train)
+
     w_train = np.ones(y_train.shape)
     w_train[y_train == 1] = signal_scale
     w_train[y_train == 0] = background_scale
 
-    # summerize events with 2D histogram
-    number_of_bins = 20
-    scale = 4
-    bins = np.linspace(-scale, scale, number_of_bins)
 
-    hist_x_train_signal = np.histogram2d(x_train_noshift_signal[:, 1], x_train_noshift_signal[:, 0], bins= [bins,bins])
-    hist_x_train_noshift_background = np.histogram2d(x_train_noshift_background[:, 1], x_train_noshift_background[:, 0], bins= [bins,bins])
-    hist_x_train_upshift_background = np.histogram2d(x_train_upshift_background[:, 1], x_train_upshift_background[:, 0], bins= [bins,bins])
-    hist_x_train_downshift_background = np.histogram2d(x_train_downshift_background[:, 1], x_train_downshift_background[:, 0], bins= [bins,bins])
+    ## Testing dataset
+    num_test = num_train
+    x_test, y_test = make_dataset(num_test, 0)
+    x_test_up, _ = make_dataset(num_test, shift)
+    x_test_down, _ = make_dataset(num_test, -shift)
+
+    w_test = np.ones(y_test.shape)
+    w_test[y_test == 1] = signal_scale
+    w_test[y_test == 0] = background_scale
+
+
+    ##  Summerize events with 2D histogram
+    number_of_bins = 20
+    bins = (number_of_bins, number_of_bins)
+    xlim = -3
+    ylim = 5
+
+    hist_x_train_signal = np.histogram2d(x_test[y_test==1][:, 0], x_test[y_test==1][:, 1], bins=bins, range=(xlim, ylim), density=True)
+    hist_x_train_noshift_background = np.histogram2d(x_test[y_test==0][:, 0], x_test[y_test==0][:, 1], bins=bins, range=(xlim, ylim), density=True)
+    hist_x_train_upshift_background = np.histogram2d(x_test_up[y_test==0][:, 0], x_test_up[y_test==0][:, 1], bins=bins, range=(xlim, ylim), density=True)
+    hist_x_train_downshift_background = np.histogram2d(x_test_down[y_test==0][:, 0],x_test_down[y_test==0][:, 1], bins=bins, range=(xlim, ylim), density=True)
 
 
     def makeplot(histograms):
@@ -88,8 +100,8 @@ def main(shift_scale, shift, plot_label):
         plt.plot([-999], [-999], lw=lw, ls=":", color="C1", alpha=0.8, label="Bkg. (down-shift)")
         plt.xlabel("$x_1$")
         plt.ylabel("$x_2$")
-        plt.xlim(limit[0], limit[1])
-        plt.ylim(limit[0], limit[1])
+        plt.xlim(xlim)
+        plt.ylim(ylim)
         plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0., prop={'size': 14})
         plt.savefig("./plots/sig_bkg_wave{}".format(plot_label), bbox_inches = "tight")
         #plt.show()
@@ -98,12 +110,13 @@ def main(shift_scale, shift, plot_label):
     makeplot([hist_x_train_signal, hist_x_train_noshift_background, hist_x_train_upshift_background, hist_x_train_downshift_background])
 
     # save training data into pickle
-    pickle.dump([x_train_noshift_signal, x_train_upshift_signal, x_train_downshift_signal, x_train_noshift_background, x_train_upshift_background, x_train_downshift_background, y_train, w_train], open("train.pickle", "wb"))
+    pickle.dump([x_train, x_train_up, x_train_down, y_train, w_train], open("train.pickle", "wb"))
+    pickle.dump([x_train, x_test_up, x_test_down, y_test, w_test], open("test.pickle", "wb"))
     pickle.dump(plot_label, open("plot_label.pickle", "wb"))
 
 if __name__ == "__main__":
     shift_scale = 1.0
-    shift = shift_scale * np.array([1.0, 1.0])
+    shift = shift_scale * np.array([0.0, 1.0])
     # labels sollten lauten: "CE_*", "SD_no_nuisance_*", "SD_with_nuisance_*"
-    plot_label = "SD_no_nuisance3"
+    plot_label = "SD_no_nuisance1"
     main(shift_scale, shift, plot_label)
