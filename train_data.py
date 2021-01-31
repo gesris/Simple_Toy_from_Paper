e6cc73a56d7959f2c8c71ba13e54a9a64f41356f
gesris <gessi.risto@gmx.de>
2021-01-31 20:59:50 +0100

    bugfixes

--- train_data.py
+++ train_data.py
@@ -275,10 +275,10 @@ def main(loss):
 
     ## prerequisites for training
     steps = []
-    max_steps = 100000
+    max_steps = 1000
     loss_train_list = []
     loss_validation_list = []
-    max_patience = 1000
+    max_patience = 30
     patience = max_patience
 
     ## initial loss:
