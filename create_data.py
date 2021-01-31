53b1d464200ee031d3970bb74e5b652d0a3f0076
gesris <gessi.risto@gmx.de>
2021-01-31 21:49:37 +0100

    changing data for different plots

--- create_data.py
+++ create_data.py
@@ -118,5 +118,5 @@ if __name__ == "__main__":
     shift_scale = 1.0
     shift = shift_scale * np.array([0.0, 1.0])
     # labels sollten lauten: "CE_*", "SD_no_nuisance_*", "SD_with_nuisance_*"
-    plot_label = "SD_with_nuisance6"
+    plot_label = "CE_1"
     main(shift_scale, shift, plot_label)
