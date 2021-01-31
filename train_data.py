9377b9b73d5e01c3e472997e3f5b2a8a189a7829
gesris <gessi.risto@gmx.de>
2021-01-31 14:23:52 +0100

    bugfixes

--- train_data.py
+++ train_data.py
@@ -252,13 +252,14 @@ def main(loss):
     ######################\n\
     # Warmup Initialized #\n\
     ######################\n")
-        warmup_steps = 100
+        warmup_steps = 30
         for warmup_step in range(0, warmup_steps):
             ## Warmup trains model without nuisance to increase stability
             grads = grad_sd([mu, theta], with_nuisance=False)    # nuisance has to be FALSE here
             loss = loss_sd([mu, theta], with_nuisance=False, training=True)
             optimizer.apply_gradients(zip(grads, model.trainable_variables))
-            print("Warmup Step: {:02d}/{},         Loss: {:.4f}".format(warmup_step, warmup_steps, loss))
+            if warmup_step % 5 == 0:
+                print("Warmup Step: {:02d}/{},         Loss: {:.4f}".format(warmup_step, warmup_steps, loss))
 
     
 
