bdc912e8461cc5d3c77f6c67de386ae97231c045
gesris <gessi.risto@gmx.de>
2021-02-01 11:59:11 +0100

    changing data for different plots

--- create_data.py
+++ create_data.py
@@ -9,7 +9,7 @@ import pickle
 
 # Make signal and background samples with up and down shifts in background process
 def make_sig(num_events):
-    mean = [0.0, 0.0]
+    mean = [0, 0]
     corr = -0.0
     cov = [[1.0, corr], [corr, 1.0]]
     return np.random.multivariate_normal(mean, cov, num_events)
@@ -69,45 +69,43 @@ def main(shift_scale, shift, plot_label):
 
 
     ##  Summerize events with 2D histogram
-    number_of_bins = 20
-    bins = (number_of_bins, number_of_bins)
     xlim = (-3, 5)
     ylim = (-3, 5)
 
-    hist_x_train_signal = np.histogram2d(x_test[y_test==1][:, 1], x_test[y_test==1][:, 0], bins=bins, range=(xlim, ylim), density=True)
-    hist_x_train_noshift_background = np.histogram2d(x_test[y_test==0][:, 1], x_test[y_test==0][:, 0], bins=bins, range=(xlim, ylim), density=True)
-    hist_x_train_upshift_background = np.histogram2d(x_test_up[y_test==0][:, 1], x_test_up[y_test==0][:, 0], bins=bins, range=(xlim, ylim), density=True)
-    hist_x_train_downshift_background = np.histogram2d(x_test_down[y_test==0][:, 1],x_test_down[y_test==0][:, 0], bins=bins, range=(xlim, ylim), density=True)
+    plt.figure(figsize=(6, 6))
+    bins = (20, 20)
+    xlim = (-3, 5)
+    ylim = (-3, 5)
+    h_sig, xe, ye = np.histogram2d(x_test[y_test == 1][:, 0], x_test[y_test == 1][:, 1],
+            bins=bins, range=(xlim, ylim), density=True)
+    h_bkg, _, _ = np.histogram2d(x_test[y_test == 0][:, 0], x_test[y_test == 0][:, 1],
+            bins=bins, range=(xlim, ylim), density=True)
+    h_bkg_up, _, _ = np.histogram2d(x_test_up[y_test == 0][:, 0], x_test_up[y_test == 0][:, 1],
+            bins=bins, range=(xlim, ylim), density=True)
+    h_bkg_down, _, _ = np.histogram2d(x_test_down[y_test == 0][:, 0], x_test_down[y_test == 0][:, 1],
+            bins=bins, range=(xlim, ylim), density=True)
+    norm=plt.Normalize(0,1)
+    cmap1 = matplotlib.colors.LinearSegmentedColormap.from_list("", ["C0"] * 3)
+    cmap2 = matplotlib.colors.LinearSegmentedColormap.from_list("", ["C1"] * 3)
+    lw = 2
+    plt.contour(h_bkg.T, extent=[xe[0], xe[-1], ye[0], ye[-1]], linewidths=lw, cmap=cmap2, alpha=1.0)
+    plt.contour(h_bkg_up.T, extent=[xe[0], xe[-1], ye[0], ye[-1]], linestyles="--", linewidths=lw, cmap=cmap2, alpha=0.7)
+    plt.contour(h_bkg_down.T, extent=[xe[0], xe[-1], ye[0], ye[-1]], linestyles=":", linewidths=lw, cmap=cmap2, alpha=0.7)
+    plt.contour(h_sig.T, extent=[xe[0], xe[-1], ye[0], ye[-1]], linewidths=lw, cmap=cmap1, alpha=1.0)
+    plt.plot([-999], [-999], lw=lw, color="C0", label="Signal")
+    plt.plot([-999], [-999], lw=lw, color="C1", label="Background")
+    plt.plot([-999], [-999], lw=lw, ls="--", color="C1", alpha=0.8, label="Bkg. (up-shift)")
+    plt.plot([-999], [-999], lw=lw, ls=":", color="C1", alpha=0.8, label="Bkg. (down-shift)")
+    plt.xlabel("$x_1$")
+    plt.ylabel("$x_2$")
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    ticks = [-2, 0, 2, 4]
+    plt.gca().set_xticks(ticks)
+    plt.gca().set_yticks(ticks)
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0., prop={'size': 14})
+    plt.savefig("./plots/sig_bkg_wave_{}".format(plot_label), bbox_inches = "tight")
 
-
-    def makeplot(histograms):
-        limit = [-4, 4]
-        plt.figure(figsize=(6, 6))
-        cmap_sig = matplotlib.colors.LinearSegmentedColormap.from_list("", ["C0"] * 3)
-        cmap_bkg = matplotlib.colors.LinearSegmentedColormap.from_list("", ["C1"] * 3)
-        cmap = [cmap_sig, cmap_bkg, cmap_bkg, cmap_bkg]
-        color=["C0", "C1", "C1",  "C1"]
-        label=["Signal", "Background", "Bkg. upshift", "Bkg. downshift"]
-        ls = ["solid", "solid", "dashed", "dotted"]
-        lw = 2
-        alpha = [0.8, 0.8, 0.6, 0.6]
-        for i in range(0, len(histograms)):
-            plt.contour(histograms[i][0], extent= [histograms[i][1][0], histograms[i][1][-1], histograms[i][2][0] , histograms[i][2][-1]], cmap=cmap[i], linewidths=2, linestyles=ls[i], alpha=alpha[i])
-            plt.plot([-999], [-999], color=color[i])
-        plt.plot([-999], [-999], lw=lw, color="C0", label="Signal")
-        plt.plot([-999], [-999], lw=lw, color="C1", label="Background")
-        plt.plot([-999], [-999], lw=lw, ls="--", color="C1", alpha=0.8, label="Bkg. (up-shift)")
-        plt.plot([-999], [-999], lw=lw, ls=":", color="C1", alpha=0.8, label="Bkg. (down-shift)")
-        plt.xlabel("$x_1$")
-        plt.ylabel("$x_2$")
-        plt.xlim(xlim)
-        plt.ylim(ylim)
-        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0., prop={'size': 14})
-        plt.savefig("./plots/sig_bkg_wave_{}".format(plot_label), bbox_inches = "tight")
-        #plt.show()
-
-
-    makeplot([hist_x_train_signal, hist_x_train_noshift_background, hist_x_train_upshift_background, hist_x_train_downshift_background])
 
     # save training data into pickle
     pickle.dump([x_train, x_train_up, x_train_down, y_train, w_train], open("train.pickle", "wb"))
@@ -118,5 +116,5 @@ if __name__ == "__main__":
     shift_scale = 1.0
     shift = shift_scale * np.array([0.0, 1.0])
     # labels sollten lauten: "CE_*", "SD_no_nuisance_*", "SD_with_nuisance_*"
-    plot_label = "CE_2"
+    plot_label = "CE_1"
     main(shift_scale, shift, plot_label)
