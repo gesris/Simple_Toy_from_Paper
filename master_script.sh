50446ad81bfa9f29d40bae5073aaa18a7815e7d6
gesris <gessi.risto@gmx.de>
2021-02-01 14:27:19 +0100

    bugfixes

--- master_script.sh
+++ master_script.sh
@@ -3,5 +3,5 @@
 ssh -t gristo@deepthought 'cd Simple_Toy_from_Paper && git pull && sh master_plot.sh'
 ssh -t gristo@bms3 'cd Simple_Toy_from_Paper && sh master_fit.sh'
 
-scp bms3:/home/gristo/Simple_Toy_from_Paper/plots/* /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/
-scp bms3:/home/gristo/Simple_Toy_from_Paper/*.png /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/
+scp bms3:/home/gristo/Simple_Toy_from_Paper/plots/* /home/risto/Masterarbeit/Simple_Toy_from_Paper/plots/
+scp bms3:/home/gristo/Simple_Toy_from_Paper/*.png /home/risto/Masterarbeit/Simple_Toy_from_Paper/plots/
