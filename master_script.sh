0f10874d54c175b1bd13006f210db6026bc23d00
gesris <gessi.risto@gmx.de>
2021-02-01 16:27:19 +0100

    bugfixes

--- master_script.sh
+++ master_script.sh
@@ -17,8 +17,8 @@ do
     git commit -m "bugfixes" .
     git push origin test
 
-    ssh -t -t gristo@deepthought 'cd Simple_Toy_from_Paper && git checkout test && git pull && sh master_plot.sh'
-    # ssh -t -t gristo@bms3 'cd Simple_Toy_from_Paper && sh master_fit.sh'
+    # ssh -t -t gristo@deepthought 'cd Simple_Toy_from_Paper && git checkout test && git pull && sh master_plot.sh'
+    ssh -t -t gristo@bms3 'cd Simple_Toy_from_Paper && sh master_fit.sh'
 
     #scp bms3:/home/gristo/Simple_Toy_from_Paper/plots/* /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/
     #scp bms3:/home/gristo/Simple_Toy_from_Paper/*.png /home/risto/Masterarbeit/Simple_Toy_from_Paper/Plots/plot_nll_$PLOTLABEL.png
